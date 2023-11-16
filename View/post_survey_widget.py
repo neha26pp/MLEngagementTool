2a00b5bcd84ebe068b7110c2a352698e88f802a6
yuying-jin <juziyuyu@sina.com>
2023-11-16 13:29:59 -0500

    remove "View."

--- View/post_survey_widget.py
+++ View/post_survey_widget.py
@@ -6,7 +6,7 @@ from PyQt5.QtMultimediaWidgets import QVideoWidget
 from PyQt5.QtMultimedia import QMediaContent, QMediaPlayer
 from PyQt5.QtCore import QTimer
 
-from View.header_widget import HeaderWidget
+from header_widget import HeaderWidget
 
 file_path = os.path.join(os.path.dirname(__file__), "..", "quiz_data", "responses.txt")
 
